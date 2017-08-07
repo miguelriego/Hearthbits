05291decbef28c5676cc798fe3b2a7005322b5c8
Miguel Angel Riego Alrich <miguel.a.riego@gmail.com>
2017-08-07 09:26:23 -0500

    Update inbot.py

--- src/inbot.py
+++ src/inbot.py
@@ -7,13 +7,13 @@ import re
 from telebot import *
 from soundbot import *
 
-API_TOKEN = '428829269:AAHku3MWFL02S2J5Ira5dWHj-koIbwBCkHg'
+API_TOKEN = ''
 
 bot = telebot.TeleBot(API_TOKEN)
 telebot.logger.setLevel(logging.DEBUG)
 
 
-# WORKS: Builds inline card query function. Using InlineQueryResultArticle while I figure how to give cards format
+# Inline card query function. Using InlineQueryResultArticle while I figure how to give cards format
 @bot.inline_handler(lambda query: len(query.query) > 0)
 def query_card(inline_query):
     temp_list = []
@@ -21,14 +21,13 @@ def query_card(inline_query):
         sound_dict = scrape(inline_query.query)
         for k, v in sound_dict.items():
             if k != 'Name':
-                print(k,v)
                 temp_list.append(types.InlineQueryResultArticle(k, k, types.InputTextMessageContent(sound_dict['Name']+"\'s ["+k+"] bit:\n"+v), None, 'http://telegram.org', True,  'Subtitle 1', 'https://telegram.org/img/t_logo.png',640, 640))
                 #temp_list.append(types.InlineQueryResultVoice(id=k, voice_url=v, title="^ "+sound_dict['Name']+"\'s ["+k+"] bit:\n"+v, caption=sound_dict['Name']+"\'s ["+k+"] bit"))
         bot.answer_inline_query(inline_query.id, temp_list, cache_time=1)
     except Exception as e:
             print(e)
 
-
+# Inline card query used when nothing is typed yet
 @bot.inline_handler(lambda query: len(query.query) is 0)
 def default_query(inline_query):
     try:
